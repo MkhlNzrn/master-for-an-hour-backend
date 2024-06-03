4ed7723c0053e69412f95774853651b96e6e7880
Miqael Nazarian <mikanaz1203@gmail.com>
2024-06-03 17:37:31 +0300

    feat: add MasterRepository

---
+++ src/main/java/org/example/repositories/MasterRepository.java
@@ -0,0 +1,14 @@
+package org.example.repositories;
+
+import org.example.entites.Master;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MasterRepository extends JpaRepository<Master, Long> {
+    @Query(value = "select m from Master m")
+    Page<Master> findAllMastersPage(Pageable pageable);
+}
