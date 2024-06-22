2abb8dd230046211163e2931b9ef9b037ccde7c6
Miqael Nazarian <mikanaz1203@gmail.com>
2024-06-23 00:50:09 +0300

    fix: hot fix

--- src/main/java/org/example/configs/SecurityConfiguration.java
+++ src/main/java/org/example/configs/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
                     return corsConfiguration;
                 }))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/auth/**", "/masters/info/{id}", "/masters/", "/masters/documents", "/masters/photo","/tasks/").permitAll()
+                        .requestMatchers("/auth/**", "/masters/info/{id}", "/masters/", "/masters/documents", "/masters/photo","/masters/metro-stations","/tasks/").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/swagger-ui/**", "/swagger-resources/*", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
